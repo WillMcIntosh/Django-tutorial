502695ce84b745195a85670d1fd30615366be11c
WillMcIntosh <william.mcintosh@ranenetwork.com>
2018-06-25 13:31:57 -0400

    finished part 2 and started to work with templates.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
@@ -6,13 +9,18 @@ class Question(models.Model):
     # ...
     def __str__(self):
         return self.question_text
+
+    # custom method for demo
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
     # ...
     def __str__(self):
-        return self.question_text
+        return self.choice_text
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)

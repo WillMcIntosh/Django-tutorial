0e443711d8d0344de27cf4bdff937a2632560f11
WillMcIntosh <william.mcintosh@ranenetwork.com>
2018-06-26 11:25:41 -0400

    finished part 6 of tutorial and edited urls.py to show static files and remove 404 error.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
         path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
         path('<int:question_id>/vote', views.vote, name='vote'),
 ]
+
+# for localhost static files
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
